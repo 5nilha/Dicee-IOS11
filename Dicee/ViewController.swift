d5077269e9aa5a760d082b3505efaedfc0a54a2a
5nilha <bwtdirect@gmail.com>
2017-11-26 01:58:56 -0500

    Final commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Fabio Quintanilha on 11/19/17.
+//  Copyright © 2017 Fabio Quintanilha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateDices()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDices()
+    }
+    
+    func updateDices(){
+        
+        //Generate a random number between 0 and 5
+        randomDiceIndex1 = Int(arc4random_uniform(6)) // Cast the arc4random to Int
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+    //Motion when shake the phone
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        updateDices()
+    }
+}
+
